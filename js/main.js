3a26f06957d801842f113f721d6f459e6ee3ea3a
juanpchcia <juanpchica@hotmail.com>
2019-03-17 21:45:27 -0500

    Se agrega formulario como buscador

--- js/main.js
+++ js/main.js
@@ -3,5 +3,14 @@ jQuery(document).ready(function($) {
 
 	/* Menu */
 	$('nav#menu').mmenu();
+	
+	$(".js-range-slider").ionRangeSlider({
+        min: 0,
+        max: 100,
+        from: 0,
+        to: 100,
+        grid: true,
+        skin: "modern"
+	});
 
 });
